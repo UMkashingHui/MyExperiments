d721c14050182903f2448eca8f91ca6ced179bbb
KevinXu <kevin0416@gmail.com>
2023-01-17 17:07:18 +0800

    first commit

---
+++ mbp_opencv/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat srcImage = imread("../test.jpeg");
+    if (!srcImage.data) {
+        std::cout << "Image not loaded";
+        return -1;
+    }
+    imshow("image", srcImage);
+    waitKey(0);
+    return 0;
+}
